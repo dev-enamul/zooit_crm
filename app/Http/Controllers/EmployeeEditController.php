8659ae2489881f8c3bc139091aac7842aeda61ad
Md Enamul Haque <enamul807849@gmail.com>
2024-03-21 13:20:05 +0600

    update

--- app/Http/Controllers/EmployeeEditController.php
+++ app/Http/Controllers/EmployeeEditController.php
@@ -49,8 +49,7 @@ class EmployeeEditController extends Controller
         DB::beginTransaction();
         try{   
             $user_reporting = ReportingUser::where('user_id', $id)->where('status',1)->latest()->first();
-
-            dd($user_reporting->id);
+ 
             if($user_reporting != null){
                 $user_reporting->status = 0;
                 $user_reporting->deleted_by = auth()->user()->id; 
