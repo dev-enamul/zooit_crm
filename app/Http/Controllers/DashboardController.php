efd9ded589e81d689f7bce6f5ec8a5f888bbf1fb
Md Enamul Haque <enamul807849@gmail.com>
2024-03-25 14:10:31 +0600

    update

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -47,7 +47,7 @@ class DashboardController extends Controller
 
     public function index(){ 
 
-        $user = User::latest()->first();
+        $user = User::latest()->take(10)->get();
         dd($user);
         $reporting = ReportingUser::Latest()->first();
         $reporting->update(['reporting_user_id'=>1]);
