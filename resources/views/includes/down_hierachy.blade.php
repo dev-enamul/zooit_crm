27a6f13c0890f3d988b33a64aaeb84175121468a
Md Enamul Haque <enamul807849@gmail.com>
2024-03-28 14:15:40 +0600

    update hairacy

--- resources/views/includes/down_hierachy.blade.php
+++ resources/views/includes/down_hierachy.blade.php
@@ -5,17 +5,15 @@
 </style> --}}
 <ul>
     @foreach ($organogram['downlines'] as $downline) 
-        @if (@$downline['user']->user->user_type==1)
-        <li>
-            <a href="{{route('employees.hierarchy',['employee'=> @$downline['user']->user->id])}}">
-                <img src="{{@$downline['user']->user->image()}}">
-                <span>{{ @$downline['user']?->user?->name }} <br>{{ @$downline['user']?->user?->user_id }} </span>
-            </a>   
-            @if (!empty($downline['downlines'])) 
-                @include('includes.down_hierachy', ['organogram' => $downline,'depth' => $depth - 1])
-            @endif
-        </li>  
-        @endif  
+    <li>
+        <a href="{{route('employees.hierarchy',['employee'=> @$downline['user']->user->id])}}" style="{{!empty($downline['downlines'])?'background:#ddd':''}}">
+            <img src="{{@$downline['user']->user->image()}}">
+            <span>{{ @$downline['user']?->user?->name }} <br>{{ @$downline['user']?->user?->user_id }} </span>
+        </a>   
+        @if (!empty($downline['downlines']) && $depth > 1) 
+            @include('includes.down_hierachy', ['organogram' => $downline,'depth' => $depth - 1])
+        @endif
+    </li>  
     @endforeach 
 </ul>
  
