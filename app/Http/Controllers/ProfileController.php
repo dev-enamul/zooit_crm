8edc95849c68989beaa1183a3dbb853e814028c2
Md Enamul Haque <enamul807849@gmail.com>
2024-04-19 10:19:21 +0600

    update

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -29,11 +29,16 @@ class ProfileController extends Controller
         return view('profile.hierarchy',compact('user_id','organogram','user'));
     }
 
-    public function profile($id){
+    public function profile($id){ 
+        dd("yes");
         $user_id = decrypt($id); 
         $user = User::find($user_id); 
 
         $reporting_users = user_reporting($user_id);
+
+        $data = ReportingUser::where('user_id', $user_id)->where('status',1)->latest()->first();
+        $boss = ReportingUser::find($data->reporting_user_id);
+        dd($boss);
         
         if(count($reporting_users) > 1){
             $reporting_user = user_info($reporting_users[1]);
