aac162c808e60a64ec0cab0e49e115bfa14162c0
Md Enamul Haque <enamul807849@gmail.com>
2024-04-22 12:41:09 +0600

    changed user

--- routes/web.php
+++ routes/web.php
@@ -432,7 +432,8 @@ Route::get('/migrate-refresh', [DashboardController::class, 'migrate_fresh']);
 
 Route::get('function_test', function () {  
         $reporting_user = ReportingUser::find(4354);
-        dd($reporting_user);
+        $user = User::find($reporting_user->user_id);
+        dd($user);
 
         $data = User::where('phone','01713552903')->first(); 
         $employees = User::where('serial','>',$data->serial)->where('user_type',1)->get(); 
