fbb323b39c306d69b3fe0255a188e63e56d0dfcf
Md Enamul Haque <enamul807849@gmail.com>
2024-04-24 10:25:15 +0600

    changed

--- routes/web.php
+++ routes/web.php
@@ -436,6 +436,10 @@ Route::get('/migrate-refresh', [DashboardController::class, 'migrate_fresh']);
 
 Route::get('function_test', function () {  
 
+        $user = User::where('phone','01329656325')->first(); 
+
+        $customers = Customer::where('created_by',$user->id)->get();
+        dd($customers);
         $user_reporting = ReportingUser::latest()->first();
         dd($user_reporting);
           
