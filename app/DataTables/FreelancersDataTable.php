710b6881db5513f57692db0d66ac90757688dcea
Md Enamul Haque <enamul807849@gmail.com>
2024-05-08 16:57:32 +0600

    change freelancer

--- app/DataTables/FreelancersDataTable.php
+++ app/DataTables/FreelancersDataTable.php
@@ -113,8 +113,8 @@ class FreelancersDataTable extends DataTable
                 $start_date = date('Y-m-d',strtotime($date[0]));
                 $end_date = date('Y-m-d',strtotime($date[1]));
                 $model = $model->whereBetween('created_at',[$start_date.' 00:00:00',$end_date.' 23:59:59']);
-            }else{
-                $model = $model->whereBetween('created_at',[date('Y-m-d').' 00:00:00',date('Y-m-d').' 23:59:59']);
+            }else{ 
+                $model = $model->whereBetween('created_at',[date('Y-m-01').' 00:00:00',date('Y-m-t').' 23:59:59']);
             }
         }
 
