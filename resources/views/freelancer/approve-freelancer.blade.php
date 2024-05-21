9d0780df715ef8d67b6cb7dcef5f0c4646ec2b39
Md Enamul Haque <enamul807849@gmail.com>
2024-05-21 09:55:07 +0600

    changed

--- resources/views/freelancer/approve-freelancer.blade.php
+++ resources/views/freelancer/approve-freelancer.blade.php
@@ -6,19 +6,18 @@
 @section('content')
     <div class="main-content">
         <div class="page-content">
-            <div class="container-fluid">
-
+            <div class="container-fluid"> 
                 <!-- start page title -->
                 <div class="row">
                     <div class="col-12">
                         <div class="page-title-box d-flex align-items-center justify-content-between">
-                            <h4 class="mb-sm-0">Freelancer List</h4>
-
+                            <h4 class="mb-sm-0">{{$title}}</h4>
                             <div class="page-title-right">
-                                <ol class="breadcrumb m-0">
-                                    <li class="breadcrumb-item"><a href="javascript: void(0);">Dashboard</a></li>
-                                    <li class="breadcrumb-item active">Freelancer List</li>
-                                </ol>
+                                <div class="btn-group flex-wrap mb-2">      
+                                    <button class="btn btn-secondary" data-bs-toggle="offcanvas" data-bs-target="#offcanvas">
+                                        <span><i class="fas fa-filter"></i> Filter</span>
+                                    </button> 
+                                </div>
                             </div>
 
                         </div>
@@ -26,119 +25,62 @@
                 </div>
                 <!-- end page title -->
 
-
-
                 <div class="row">
                     <div class="col-12">
-                        <div class="card">
-                            <div class="card-body">
+                        <div class="card"> 
+                            <div class="card-body"> 
                                 <div class="table-box">
-                                    <table id=" "
-                                        class="table table-hover table-bordered table-striped dt-responsive nowrap"
-                                        style="border-collapse: collapse; border-spacing: 0; width: 100%;">
-                                        <thead>
-                                            <tr class="">
-                                                <th>Action</th>
-                                                <th>S/N</th>
-                                                <th>Fl Id</th>
-                                                <th>Name</th>
-                                                <th>Mobile</th>
-                                                <th>Co-Ordinator Name_ID</th>
-                                                <th>Executive Co-Ordinator Name_ID</th>
-                                                <th>InCharge Sales</th>
-                                                <th>Area InCharge</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            @foreach ($datas as $key => $data)
-                                                <tr class="">
-                                                    <td class="text-center" data-bs-toggle="tooltip" title="Action">
-                                                        <div class="dropdown">
-                                                            <a href="javascript:void(0)" data-bs-toggle="dropdown"
-                                                                aria-expanded="false">
-                                                                <img class="rounded avatar-2xs p-0"
-                                                                    src="{{ $data->user->image() }}">
-                                                            </a>
-                                                            <div class="dropdown-menu dropdown-menu-animated">
-                                                                <a class="dropdown-item"
-                                                                    href="{{ route('profile', encrypt($data?->user_id)) }}">Profile</a>
-                                                                <a class="dropdown-item" href="javascript:void(0)"
-                                                                    onclick="approveFreelancer({{ $data->user_id }})">Approve</a>
-                                                            </div>
-                                                        </div>
-                                                    </td>
-                                                    <td>{{ $key + 1 }}</td>
-                                                    <td>{{ @$data->user->user_id }}</td>
-                                                    <td>{{ @$data->user->name }}</td>
-                                                    <td>{{ @$data->user->phone }}</td>
-                                                    <td>
-                                                        @php
-                                                            $userInfo = coOrdinator(
-                                                                json_decode($data->user->user_reporting),
-                                                            );
-                                                        @endphp
-                                                        <center>{{ $userInfo }}<center>
-                                                    </td>
-                                                    <td>
-                                                        @php
-                                                            $userInfo = exCoOrdinator(
-                                                                json_decode($data->user->user_reporting),
-                                                            );
-                                                        @endphp
-                                                        <center>{{ $userInfo }}<center>
-                                                    </td>
-
-                                                    <td>
-                                                        @php
-                                                            $reporting = json_decode($data->user->user_reporting);
-                                                            $userInfo = '-'; // Set a default value
-                                                            if (isset($reporting) && $reporting != null) {
-                                                                $user = User::whereIn('id', $reporting)
-                                                                    ->whereHas('freelancer', function ($q) {
-                                                                        $q->whereIn('designation_id', [12, 13, 14, 15]);
-                                                                    })
-                                                                    ->first();
-                                                                if (isset($user) && $user != null) {
-                                                                    $userInfo =
-                                                                        $user->name . ' [' . $user->user_id . ']';
-                                                                }
-                                                            }
-                                                        @endphp
-                                                        <center>{{ $userInfo }}<center>
-                                                    </td>
-                                                    <td>
-                                                        @php
-                                                            $reporting = json_decode($data->user->user_reporting);
-                                                            $userInfo = '-'; // Set a default value
-                                                            if (isset($reporting) && $reporting != null) {
-                                                                $user = User::whereIn('id', $reporting)
-                                                                    ->whereHas('freelancer', function ($q) {
-                                                                        $q->whereIn('designation_id', [11]);
-                                                                    })
-                                                                    ->first();
-                                                                if (isset($user) && $user != null) {
-                                                                    $userInfo =
-                                                                        $user->name . ' [' . $user->user_id . ']';
-                                                                }
-                                                            }
-                                                        @endphp
-                                                        <center>{{ $userInfo }}<center>
-                                                    </td>
-                                                </tr>
-                                            @endforeach
-                                        </tbody>
-                                    </table>
-                                </div>
-
+                                    {{ $dataTable->table(['class' => 'table table-hover table-bordered table-striped dt-responsive nowrap']) }} 
+                                </div> 
                             </div>
                         </div>
                     </div> <!-- end col -->
                 </div>
                 <!-- end row -->
             </div> <!-- container-fluid -->
+        </div> 
+    </div>      
+    
+    <div class="offcanvas offcanvas-end" id="offcanvas">
+        <div class="offcanvas-header">
+            <h5 class="offcanvas-title">Select Filter Item</h5>
+            <button class="btn btn-label-danger btn-icon" data-bs-dismiss="offcanvas">
+                <i class="fa fa-times"></i>
+            </button>
         </div>
-        @include('includes.footer')
-    </div>
+        <div class="offcanvas-body">
+            <form action="" method="get">
+                <div class="row">  
+                    <div class="col-md-12">
+                        <div class="mb-3">
+                            <label for="status" class="form-label">Status</label>
+                            <select class="select2" id="status" name="status"> 
+                                <option value = "1" {{$status==1?"selected":""}}>Previous</option>
+                                <option value = "0" {{$status==0?"selected":""}}>Present</option>
+                            </select> 
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="mb-3">
+                            <label for="date_range" class="form-label">Date</label>
+                            <input class="form-control" start="{{$start_date}}" end="{{$end_date}}" id="date_range" name="date" default="This Month" type="text" value="" />   
+                        </div>
+                    </div> 
+                    <div class="col-md-12">
+                        <div class="mb-3">
+                            <label for="employee" class="form-label">Employee</label>
+                            <select class="select2" search id="employee" name="employee"> 
+                                <option value = "{{$employee->id}}" selected="selected">{{$employee->name}} [{{$employee->user_id}}]</option>
+                            </select> 
+                        </div>
+                    </div>   
+                    <div class="text-center">
+                        <button class="btn btn-primary" type="submit" data-bs-dismiss="offcanvas">Filter</button>
+                    </div> 
+                </div>
+            </form>
+        </div>
+    </div> 
 
     <div class="modal fade" id="approve_frelancer">
         <div class="modal-dialog modal-md">
@@ -229,5 +171,27 @@
             $('input[name="user_id"]').val(user_id);
             $('#approve_frelancer').modal('show');
         }
+    </script>
+
+    {!! $dataTable->scripts() !!}   
+    <script>  
+        $(document).ready(function() {
+                $('#employee').select2({
+                    placeholder: "Select Employee",
+                    allowClear: true,
+                    ajax: {
+                        url: '{{ route('select2.employee') }}',
+                        dataType: 'json',
+                        data: function (params) {
+                            var query = {
+                                term: params.term
+                            }
+                            return query;
+                        }
+                    }
+                });
+            });
+
+        getDateRange('date_range');
     </script>
 @endsection
