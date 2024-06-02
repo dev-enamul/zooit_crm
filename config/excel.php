adbd244588221e5b3dc9f8ab8c0869a0e34a4754
MD Enamul Haque <eng.enamulcse@gmail.com>
2024-06-02 15:26:11 +0600

    chenge

--- config/excel.php
+++ config/excel.php
@@ -177,21 +177,21 @@ return [
     |
     */
     'extension_detector' => [
-        // 'xlsx'     => Excel::XLSX,
-        // 'xlsm'     => Excel::XLSX,
-        // 'xltx'     => Excel::XLSX,
-        // 'xltm'     => Excel::XLSX,
-        // 'xls'      => Excel::XLS,
-        // 'xlt'      => Excel::XLS,
-        // 'ods'      => Excel::ODS,
-        // 'ots'      => Excel::ODS,
-        // 'slk'      => Excel::SLK,
-        // 'xml'      => Excel::XML,
-        // 'gnumeric' => Excel::GNUMERIC,
-        // 'htm'      => Excel::HTML,
-        // 'html'     => Excel::HTML,
-        // 'csv'      => Excel::CSV,
-        // 'tsv'      => Excel::TSV,
+        'xlsx'     => Excel::XLSX,
+        'xlsm'     => Excel::XLSX,
+        'xltx'     => Excel::XLSX,
+        'xltm'     => Excel::XLSX,
+        'xls'      => Excel::XLS,
+        'xlt'      => Excel::XLS,
+        'ods'      => Excel::ODS,
+        'ots'      => Excel::ODS,
+        'slk'      => Excel::SLK,
+        'xml'      => Excel::XML,
+        'gnumeric' => Excel::GNUMERIC,
+        'htm'      => Excel::HTML,
+        'html'     => Excel::HTML,
+        'csv'      => Excel::CSV,
+        'tsv'      => Excel::TSV,
 
         /*
         |--------------------------------------------------------------------------
@@ -202,7 +202,7 @@ return [
         | Available options: Excel::MPDF | Excel::TCPDF | Excel::DOMPDF
         |
         */
-        // 'pdf'      => Excel::DOMPDF,
+        'pdf'      => Excel::DOMPDF,
     ],
 
     /*
